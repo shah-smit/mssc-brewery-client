c8ea6eed7f5938785a6be26f62cb14cfb7e3258c
Smit <smitshah95@hotmail.com>
2020-10-17 10:39:37 +0800

    Updated Mapping annotation

--- src/main/java/udemy/course/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/udemy/course/msscbreweryclient/web/client/BreweryClient.java
@@ -3,24 +3,26 @@ package udemy.course.msscbreweryclient.web.client;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import udemy.course.msscbreweryclient.web.model.BeerDto;
 
 @FeignClient(value = "breweryClient", url = "${sfg.brewery.host}")
 public interface BreweryClient {
 
-  @RequestMapping(method = RequestMethod.GET, value = "${sfg.brewery.beer-path}/{beerId}")
+  @GetMapping(value = "${sfg.brewery.beer-path}/{beerId}")
   ResponseEntity<BeerDto> getBear(@PathVariable("beerId") UUID beerId);
 
-  @RequestMapping(method = RequestMethod.POST, value = "${sfg.brewery.beer-path}")
+  @PostMapping(value = "${sfg.brewery.beer-path}")
   void addBeer(@RequestBody BeerDto beerDto);
 
-  @RequestMapping(method = RequestMethod.PUT, value = "${sfg.brewery.beer-path}/{beerId}")
+  @PutMapping(value = "${sfg.brewery.beer-path}/{beerId}")
   void updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto);
 
-  @RequestMapping(method = RequestMethod.DELETE, value = "${sfg.brewery.beer-path}/{beerId}")
+  @DeleteMapping(value = "${sfg.brewery.beer-path}/{beerId}")
   void deleteBeer(@PathVariable("beerId") UUID beerId);
 }
